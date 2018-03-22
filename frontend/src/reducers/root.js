6010cf27e5d58be6ac7773bdd63bdd924460745f
DedicatedWebStar <yam.ichiro@yandex.com>
2018-03-22 04:00:38 +0100

    root added

---
+++ frontend/src/reducers/root.js
@@ -0,0 +1,12 @@
+// Imports
+import { combineReducers } from 'redux';
+
+// App Imports
+import user from './user';
+import { tweets, tweet } from './tweets';
+
+export default combineReducers({
+    user,
+    tweets,
+    tweet
+});
