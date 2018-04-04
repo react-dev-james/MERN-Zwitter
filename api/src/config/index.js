e0dd8af8207aa237984f7dca155fa3b4be15c5f4
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-04 03:27:38 +0200

    Added config file

---
+++ api/src/config/index.js
@@ -0,0 +1,11 @@
+// src / config / index.js
+'use strict';
+
+const config = {
+    port: 5001,
+    secret: 'super-secret-key',
+    databaseUrl: 'mongodb://localhost/zwitter',
+    saltRounds: 10
+};
+
+module.exports = config;
