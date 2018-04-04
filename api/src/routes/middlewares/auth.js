687eb816426022341a503f315efcb037efc566c7
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-04 03:30:08 +0200

    authfile added

---
+++ api/src/routes/middlewares/auth.js
@@ -0,0 +1,23 @@
+// src / routes / middlewares / auth.js
+'use strict';
+
+// Imports
+const jwt = require('jsonwebtoken');
+
+const config = require('../../config/index');
+
+// Auth Middleware
+let authMiddleware = function (request, response, next) {
+    let token = request.body.token || request.query.token || request.headers['x-access-token'] || request.cookies.token;
+
+    if(token && token != 'null') {
+        request.user = jwt.verify(token, config.secret);
+    } else {
+        request.user = {};
+    }
+
+    next();
+};
+
+// Export
+module.exports = authMiddleware;
