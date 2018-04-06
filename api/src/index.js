d07e98395f87530c5d5cb57fe3c5cab8785efc91
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-06 04:33:52 +0200

    idnexjs file added

---
+++ api/src/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Imports
+const express = require('express');
+
+const config = require('./config');
+let appServer = require('./server');
+
+// Boot Server
+const port = process.env.PORT || config.port;
+
+appServer.listen(port, () => {
+    console.log('Server running on', port);
+});
