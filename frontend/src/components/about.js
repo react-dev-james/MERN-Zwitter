59ee79d0de8e91ca5eb7b45ebc453eb84801346a
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-06 04:29:54 +0200

    oters added

---
+++ frontend/src/components/about.js
@@ -0,0 +1,21 @@
+// Imports
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <section>
+                <h2>ℹ️ About</h2>
+
+                <br/>
+
+                <p>Github Repo: <a href="https://github.com/DedicatedWebStar/MERN-Zwitter" target="_blank">https://github.com/DedicatedWebStar/MERN-Zwitter</a></p>
+
+                <br/>
+
+            </section>
+        )
+    }
+}
+
+export default About;
