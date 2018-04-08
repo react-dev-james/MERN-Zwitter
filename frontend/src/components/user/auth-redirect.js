91bdb4eea53b51681510fa10ffb7d93689b7e241
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-08 04:06:41 +0200

    auth added

---
+++ frontend/src/components/user/auth-redirect.js
@@ -0,0 +1,29 @@
+// Imports
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+class AuthRedirect extends Component {
+    render() {
+        const { isAuthenticated } = this.props.user;
+
+        return (
+            <div>
+                { !isAuthenticated ? <Redirect to="/" /> : '' }
+            </div>
+        );
+    }
+}
+
+AuthRedirect.propTypes = {
+    user: PropTypes.object.isRequired,
+};
+
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
+}
+
+export default connect(mapStateToProps, {})(AuthRedirect);
