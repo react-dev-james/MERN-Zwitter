f1954a4b351c1f29657c3b54b30cb56de916f78f
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-11 02:57:26 +0200

    listjs added

---
+++ frontend/src/components/tweet/list.js
@@ -0,0 +1,34 @@
+// Imports
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+
+// UI Imports
+import { Card, CardTitle } from 'material-ui/Card';
+
+function TweetList({ tweets }) {
+    const emptyMessage = (
+        <p>No tweets to show.</p>
+    );
+
+    const tweetsList = (
+        tweets.map(({ _id, text, createdAt }) => (
+            <Card key={ _id }>
+                <Link to={ `/tweet/${ _id }` }><CardTitle title={ text } subtitle={ moment(createdAt).fromNow() } /></Link>
+            </Card>
+        ))
+    );
+
+    return (
+        <div>
+            { tweets.length === 0 ? emptyMessage : tweetsList }
+        </div>
+    );
+}
+
+TweetList.propTypes = {
+    tweets: PropTypes.array.isRequired
+};
+
+export default TweetList;
