bf07d0447745481fb3b65ce833bc230e1e546df5
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-12 03:58:20 +0200

    user commit

---
+++ frontend/src/reducers/user.js
@@ -0,0 +1,23 @@
+// Imports
+import isEmpty from 'lodash/isEmpty';
+
+// App Imports
+import { USER_CURRENT_SET } from '../actions/user';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+export default (state = initialState, action = {}) => {
+    switch(action.type) {
+        case USER_CURRENT_SET:
+            return {
+                isAuthenticated: !isEmpty(action.user),
+                user: action.user
+            };
+
+        default:
+            return state;
+    }
+}
