44d2e39a73acf088cf1b2abba2fcb55234b1572d
DedicatedWebStar <yam.ichiro@yandex.com>
2018-04-13 03:59:24 +0200

    user added

---
+++ frontend/src/actions/user.js
@@ -0,0 +1,69 @@
+// Imports
+import jwtDecode from 'jwt-decode';
+
+// App Imports
+import config from '../config';
+
+export const USER_CURRENT_SET = 'USER_CURRENT_SET';
+
+export function postLogin(credentials) {
+    return dispatch => {
+        return fetch(`${ config.url.api }user/login`, {
+            method: 'post',
+
+            body: JSON.stringify(credentials),
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => {
+                if(response.ok) {
+                    return response.json();
+                }
+            })
+            .then(response => {
+                if(response.success) {
+                    const token = response.data.token;
+
+                    localStorage.setItem('token', token);
+
+                    dispatch(setCurrentUser(jwtDecode(token)));
+                }
+
+                return response;
+            });
+    }
+}
+
+export function postRegister(credentials) {
+    return dispatch => {
+        return fetch(`${ config.url.api }user/register`, {
+            method: 'post',
+
+            body: JSON.stringify(credentials),
+
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+    }
+}
+
+export function setCurrentUser(user) {
+    return {
+        type: USER_CURRENT_SET,
+        user
+    };
+}
+
+export function userLogout() {
+    return dispatch => {
+        localStorage.removeItem('token');
+
+        dispatch(setCurrentUser({}));
+
+        return { success: true };
+    }
+}
